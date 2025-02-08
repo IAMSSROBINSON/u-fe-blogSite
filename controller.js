784f1fb85d4da0f52108349f0f23e26f6c9ef01b
SSR <iamssrobinson@gmail.com>
2025-02-08 17:54:46 +0000

    Attempting to secure homepage.

--- controller.js
+++ controller.js
@@ -1,23 +1,24 @@
 console.log("Hello World!");
-import model from './model.js';
-import view from './view.js';
+console.log("pathname:", window.location.pathname);
+// import model from './model.js';
+// import view from './view.js';
 
 
 const controller = {
-    init () {
-        console.log(window.location);
-        console.log(window.location.pathname.includes("blogSite"));
+    // init () {
+    //     console.log(window.location);
+    //     console.log(window.location.pathname.includes("blogSite"));
 
-        if (window.location.pathname.includes("blogSite")) {
-            console.log("You are on the homepage");
-            model.init();
-            view.init();
+    //     if (window.location.pathname.includes("blogSite")) {
+    //         console.log("You are on the homepage");
+    //         model.init();
+    //         view.init();
 
-        }
-        else if (window.location.pathname.includes("blogTemplate")) {
-            console.log("You are on a blog template page");
-        }
-    } 
+    //     }
+    //     else if (window.location.pathname.includes("blogTemplate")) {
+    //         console.log("You are on a blog template page");
+    //     }
+    // } 
 }
 controller.init();
 export default controller;
