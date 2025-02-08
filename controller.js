ff80162e500fb5e80db8828f0c81653ddb895ceb
SSR <iamssrobinson@gmail.com>
2025-02-08 20:28:17 +0000

    Simplify data.

--- controller.js
+++ controller.js
@@ -1,46 +1,13 @@
 console.log("Hello World!");
-console.log("pathname:", window.location.pathname);
-console.log("pathname includes u-fe-blogSite:", window.location.pathname.includes("u-fe-blogSite"));
+
 import model from './model.js';
 import view from './view.js';
 
 
 const controller = {
     init () {
-        console.log(window.location);
-        console.log(window.location.pathname.includes("u-fe-blogSite"));
-
-        if (window.location.pathname.includes("u-fe-blogSite")) {
-            console.log("You are on the homepage /u-fe-blogSite");
-            model.init();
-            view.init();
-        }
-        else if (window.location.pathname.includes("blogTemplate")) {
-            console.log("You are on a blogTemplate.html page");
-        }
-        else {
-            console.log("This page is unaccounted for");
-        }
-
-        const currentlySelectedSubjectId = model.getCurrentlySelectedSubjectId();
-        console.log("currentlySelectedSubjectId:", currentlySelectedSubjectId);
-        if (currentlySelectedSubjectId) {
-            view.setCurrentlySelectedSubjectId(currentlySelectedSubjectId);
-        }
-    },
-    handleSubjects (event) {
-        console.log("handle subjects:", event);
-        const target = event?.target;
-        const classList = [...event.target?.classList];
-        console.log("target:", target);
-        console.log("target class list", target.classList);
-        if (classList.includes("subjects-list-item")) {
-            console.log("list item clicked", target);
-            console.log("list item clicked id", target.id);
-            view.setCurrentlySelectedSubjectId(target.id);
-            model.setCurrentlySelectedSubjectId(target.id);
-        }
+      
     }
-}
+};
 controller.init();
 export default controller;
--- model.js
+++ model.js
@@ -1,34 +1,9 @@
 import data from './assets/data/data.js';
 const model = {
+    data : null,
     init () {
-        console.log("model.init");
-        console.log("assets data:", data);
-        const dataFromStorage = localStorage.getItem("blogSite");
-        if (dataFromStorage) {
-            this.data = JSON.parse(dataFromStorage);
-            if (!this.data.currentlySelectedSubject) {
-                this.data.currentlySelectedSubjectId = "all";
-            }
-            console.log("Local storage data retrieved from blogSite");
-            console.log("this.data:", this.data);
-        }
-        else {
-            this.data = data;
-            this.data.currentlySelectedSubjectId = "all";
-            localStorage.setItem("blogSite", JSON.stringify(data));
-            console.log("Local storage data set to blogSite");
-            console.log("this.data:", this.data);
-        }
-    },
-    getCurrentlySelectedSubjectId() {
-        return this.data.currentlySelectedSubjectId;
-    },
-    setCurrentlySelectedSubjectId(id) {
-        this.data.currentlySelectedSubjectId = id;
-        localStorage.setItem("blogSite", JSON.stringify(this.data));
-        console.log("local storage data set:", this.data);
-    }
 
-}
+    }   
+};
 
 export default model;
