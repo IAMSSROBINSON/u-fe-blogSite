208191eb78318707de9e46141cc335b89b2de632
SSR <iamssrobinson@gmail.com>
2025-02-08 18:07:20 +0000

    Attempting to secure homepage for github pages.

--- controller.js
+++ controller.js
@@ -1,25 +1,25 @@
 console.log("Hello World!");
 console.log("pathname:", window.location.pathname);
 console.log("pathname includes u-fe-blogSite:", window.location.pathname.includes("u-fe-blogSite"));
-// import model from './model.js';
-// import view from './view.js';
+import model from './model.js';
+import view from './view.js';
 
 
 const controller = {
-    // init () {
-    //     console.log(window.location);
-    //     console.log(window.location.pathname.includes("blogSite"));
+    init () {
+        console.log(window.location);
+        console.log(window.location.pathname.includes("u-fe-blogSite"));
 
-    //     if (window.location.pathname.includes("blogSite")) {
-    //         console.log("You are on the homepage");
-    //         model.init();
-    //         view.init();
+        if (window.location.pathname.includes("u-fe-blogSite")) {
+            console.log("You are on the homepage /u-fe-blogSite");
+            model.init();
+            view.init();
 
-    //     }
-    //     else if (window.location.pathname.includes("blogTemplate")) {
-    //         console.log("You are on a blog template page");
-    //     }
-    // } 
+        }
+        else if (window.location.pathname.includes("blogTemplate")) {
+            console.log("You are on a blogTemplate.html page");
+        }
+    } 
 }
 controller.init();
 export default controller;
